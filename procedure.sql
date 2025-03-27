cff3c68f4dff9c7da741e6d6d3f70c0d4bb0a971
Campogiani Lorenzo <campogianilorenzo@gmail>
2025-03-27 14:24:59 +0100

    Modifica Lorenzo

--- procedure.sql
+++ procedure.sql
@@ -2,4 +2,5 @@ create or replace procedure saluto as
 begin 
 	dbms_output.put_line('Ciao da lorenzo');
 	dbms_output.put_line('Questa e una modifica');
+    dbms_output.put_line('Forse ce la faremo');
 end;
