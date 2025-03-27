c8225a2c790528dc57baa8860712ab4759b7bc08
Diego Paolinelli <diegopaolinelli5@gmail.com>
2025-03-27 14:24:53 +0100

    prova di conflitto

--- procedure.sql
+++ procedure.sql
@@ -2,4 +2,5 @@ create or replace procedure saluto as
 begin 
 	dbms_output.put_line('Ciao da lorenzo');
 	dbms_output.put_line('Questa e una modifica');
+    dbms_output.put_line('a stronzo');
 end;
